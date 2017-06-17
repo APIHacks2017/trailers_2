a7abd5155b73c754e94d3fbcb84ebdad7bf14125
thebrahminator <vishwa.nath@outlook.com>
2017-06-17 15:22:47 +0530

    Listed done. Payload configuring

---
+++ vishwa/filters.py
@@ -0,0 +1,5 @@
+
+
+
+def filter1():
+    return 'lolcats'
--- vishwa/views.py
+++ vishwa/views.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template, session, request, redirect, url_for
-
+import json
+import filters
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
+
+
+@app.route('/receivedata', methods=["POST"])
+def receivedata():
+    list_of_points = request.form.get('listed', '')
+    latitude = request.form.get('lat', '')
+    longitude = request.form.get('long', '')
+    type_of_location = request.form.get('type', '')
+    duration = request.form.get('duration', '')
+    duration_dict = json.loads(duration)
+    list_of_points_parsed = json.loads(list_of_points)
+    print(list_of_points_parsed[0])
+    print(list_of_points_parsed[1])
+    #print(list_of_points)
+    return list_of_points
 
 if __name__ == '__main__':
     app.run(debug=True)
