d70235a7454fe4f6de2de57e10d9951c18e3911a
thebrahminator <vishwa.nath@outlook.com>
2017-06-17 17:55:59 +0530

    kandhan's code added

--- vishwa/views.py
+++ vishwa/views.py
@@ -42,7 +42,27 @@ def permuted():
     no_of_points = request.form.get('noofdest', '')
     list_of_points_parsed = json.loads(list_of_points)
     permuted_points = filters.permutations(list_of_points_parsed,no_of_points)
-
     return 'success'
+
+@app.route('/corpus')
+def get_corpus():
+   types = ['cafe', 'restaurant']
+   formatted_types = '|'.join(types)
+   location = "13.0620724,80.2612372"
+   api_key = 'AIzaSyAgCJBw8Jlutcm9rm6fUJFt8HmcE3ZRjS8'
+   url = 'http://52.36.211.72:5555/gateway/Google%20Places%20API/1.0/place/nearbysearch/json?key={}&' \
+         'radius={}&location={}&types={}'.format(api_key, 1000, location, formatted_types)
+   x = requests.get(url, headers={'x-Gateway-APIKey': '7c71114f-35cf-4709-aae9-651dbf216fe8'})
+   response = x.content
+   response_json = json.loads(response)
+   results = response_json['results']
+   corpus = []
+   for result in results:
+       lat, long = result['geometry']['location'].items()
+       place_id = result['place_id']
+       #rating = result['rating']
+       corpus.append(dict(lat=lat[1],long=long[1], place_id=place_id))
+       return json.dumps(corpus)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
